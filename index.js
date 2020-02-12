633655a46ee81e07a407848b7954ac11354061fd
Aston Motes <astonm@gmail.com>
2020-02-12 15:47:52 -0800

    add react example

--- index.js
+++ index.js
@@ -10,7 +10,36 @@ module.exports = ({ include, write, redirect, session }) => {
 
 You are logged in as ${session.email}.
 
+<div>
+<b>Check out this cool React counter:</b>
+
+<span id="counter"></span>
+</div>
+
 <p><a href="/logout.js">Logout</a></p>
+<script src="https://unpkg.com/react@16/umd/react.production.min.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js" crossorigin></script>
+<script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+<script type="text/babel">
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {count: 0};
+    }
+
+    incr() {
+        this.setState({count: this.state.count + 1});
+    }
+
+    render() {
+        return (
+            <button onClick={this.incr.bind(this)}>{this.state.count}</button>
+        );
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('counter'));
+</script>
     `);
 
     include("footer.js");
